d3da8fd7eeef5545c547490f104715e34f4825fe
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-06 04:22:11 -0300

    arruma o problema das rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-
 import Rotas from './main/rotas'
-
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import './custom.css'
 
 class App extends React.Component{
+  
   render(){
     return(
     <div>
-      <App />
+      <Rotas />
     </div>
     )
   }
