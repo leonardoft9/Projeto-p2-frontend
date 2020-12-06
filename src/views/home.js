3bfb95709de8614a27959c56735263bbdd89f2ff
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-06 15:22:56 -0300

    corrige segurança de localStorageService.js

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import axios from 'axios'
+import UsuarioService from '../app/service/usuarioService'
+import LocalStorageService from '../app/service/localStorageService'
+
 
 class Home extends React.Component{
 
@@ -8,16 +10,21 @@ class Home extends React.Component{
         saldo: 0
     }
 
+    constructor(){
+        super()
+        this.usuarioService = new UsuarioService();
+    }
+
     componentDidMount(){
-        const usuarioLogadoString  = localStorage.getItem('_usuario_logado')
-        const usuarioLogado = JSON.parse(usuarioLogadoString)
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
     
-        axios.get(`http://localhost:8080/api/usuarios/${usuarioLogado.id}/saldo`)
-            .then(response => {
-                this.setState({saldo: response.data})
+        this.usuarioService
+            .obterSaldoPorUsuario(usuarioLogado.id)
+            .then( response => {
+                this.setState({ saldo: response.data})
             }).catch(error => {
                 console.error(error.response)
-            })
+            });
     }
 
     render(){
