8523e363095d82ec6d1768d0a78566f3343fa7a9
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-06 20:24:11 -0300

    implementa cadastro-lancamentos.js

--- src/components/selectMenu.js
+++ src/components/selectMenu.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default (props) => {
-    
-    const options = props.lista.map( (option, index) => {
-        return(
-        <option key={index} value={option.value}>{option.label}</option>
+
+    const options = props.lista.map( (option, index)  => {
+        return (
+            <option key={index} value={option.value}>{option.label}</option>
         )
     })
 
-    return(
-        <select {...props}>
+    return (
+        <select {...props}  >
             {options}
         </select>
     )
