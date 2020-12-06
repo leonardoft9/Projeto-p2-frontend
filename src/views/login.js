d8ad8dce42f8cf8144c8cc7f0deb2918f1fd0788
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-06 01:18:01 -0300

    form-group.js e formulario de login.js

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
+import FormGroup from '../components/form-group'
 
 class Login extends React.Component{
     
@@ -11,7 +12,27 @@ class Login extends React.Component{
                 <div className="col-md-6" style={{position: 'relative', left: '300px' }}>
                     <div className="bs-docs-section">
                         <Card title="Login">
-                            Meu Cartão
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup Label="Email: *" htmlFor="exampleInputEmail1">
+                                            <input type="email" 
+                                                   className="form-control" 
+                                                   id="exampleInputEmail1" 
+                                                   aria-describedby="emailHelp" 
+                                                   placeholder="Digite o Email"/>
+                                            </FormGroup>
+                                            <FormGroup Label="Senha: *" htmlFor="exampleInputPassword1">
+                                            <input type="password" 
+                                                   className="form-control" 
+                                                   id="exampleInputPassword1" 
+                                                   placeholder="Password"/>
+                                            </FormGroup>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
                         </Card>
                     </div>
                 </div>
